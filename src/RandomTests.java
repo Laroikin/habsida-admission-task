5eae57d0d52802cd02cb90d36c99cbe94e99e29e
Laroikin <said24abdi04@gmail.com>
2022-11-16 03:31:52 +0900

    feat: add random test generator

---
+++ src/RandomTests.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomTests {
+  static HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+  private static String generateRandomCorrectString() {
+    return "" + ThreadLocalRandom.current().nextInt(1, 11) + " "
+            + map.get(ThreadLocalRandom.current().nextInt(0, 4)) + " " + ThreadLocalRandom.current().nextInt(1, 11)
+            + " " + map.get(ThreadLocalRandom.current().nextInt(0, 4)) + " "
+            + ThreadLocalRandom.current().nextInt(1, 11);
+  }
+
+  public static void main(String[] args) {
+    map.put(0, "+");
+    map.put(1, "-");
+    map.put(2, "*");
+    map.put(3, "/");
+
+    for (int i = 0; i < 100; i++) {
+      String s = generateRandomCorrectString();
+      System.out.println("Test " + (i + 1) + ": " + s);
+      new Calculator(s);
+      System.out.println("\n--------------------------------\n");
+    }
+  }
+}
