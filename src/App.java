3e1c3b76d6a4c478e88d651f176cfcc3d9c697af
Laroikin <said24abdi04@gmail.com>
2022-11-17 07:34:16 +0900

    fix: add missing prompt

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        System.out.printf("Input: ");
         Calculator obj = new Calculator(scanner.nextLine());
         obj.calculate();
         scanner.close();
